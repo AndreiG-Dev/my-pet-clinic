1a6262170f43d4669f047820b583925382f3b471
Stan Clarke <1991.andrei@gmail.com>
2021-07-20 17:01:58 +0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/springproject/mypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springproject.mypetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/springproject/mypetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springproject.mypetclinic.model;
+
+public class Vet extends Person {
+}
