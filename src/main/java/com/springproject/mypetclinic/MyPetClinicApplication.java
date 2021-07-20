8b17a49891c7d1db8d2d26bfc197b61a4b18cfd8
Stan Clarke <1991.andrei@gmail.com>
2021-07-20 15:58:40 +0300

    Initial project add

---
+++ src/main/java/com/springproject/mypetclinic/MyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetClinicApplication.class, args);
+    }
+
+}
