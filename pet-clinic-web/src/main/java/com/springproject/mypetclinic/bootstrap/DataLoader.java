eb71a563652e689d2fcb192b86edd3ebcbe8e9c3
Stan Clarke <1991.andrei@gmail.com>
2021-07-24 20:42:00 +0300

    Updated VetController and View. Closes #15

--- pet-clinic-web/src/main/java/com/springproject/mypetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springproject/mypetclinic/bootstrap/DataLoader.java
@@ -65,6 +65,7 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
+        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 //        vet1.getSpecialities().add(savedRadiology);
@@ -72,6 +73,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
+        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 //        vet2.getSpecialities().add(savedSurgery);
--- pet-clinic-web/src/main/java/com/springproject/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springproject/mypetclinic/controllers/VetController.java
@@ -1,15 +1,23 @@
 package com.springproject.mypetclinic.controllers;
 
+import com.springproject.mypetclinic.sevices.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"", "/index"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/index"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
