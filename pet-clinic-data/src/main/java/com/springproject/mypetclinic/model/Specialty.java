662b5d45d88aa3c5d95d1368114ae1bea697d703
Stan Clarke <1991.andrei@gmail.com>
2021-08-03 16:11:16 +0300

    Added Specialties. Closes #27

---
+++ pet-clinic-data/src/main/java/com/springproject/mypetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package com.springproject.mypetclinic.model;
+
+public class Specialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
